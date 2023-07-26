812b422bc4a3567fef47200abc2618bdbf410f3b
Anastasiya <vipstop2303@mail.ru>
2023-07-26 17:16:08 +0300

    задача14

---
+++ Задача14/Program.cs
@@ -0,0 +1,20 @@
+﻿// 14. Напишите программу, которая принимает 
+// на вход число и проверяет, кратно ли оно 
+// одновременно 7 и 23.
+// 14 -> нет 
+// 46 -> нет 
+// 161 -> да
+
+
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine() ?? "");
+int a = 7;
+int b = 23;
+int с = (number % a) + (number % b);
+
+if (с == 0)
+    Console.WriteLine("Число кратно 7 и 23");
+else
+{
+    Console.WriteLine("Число не кратно 7 и 23");
+}
